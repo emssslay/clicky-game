787d8a24346045e63b27354c1aabccf2a3cecf06
emssslay <emily.robinson@iechs.org>
2019-06-23 16:05:39 -0600

    trying to fix images

--- src/App.js
+++ src/App.js
@@ -13,8 +13,8 @@ function App() {
       id={1}
       width={100}
       height={100}
-      back={'./img'}
-      front={'./img'}
+      back={'../public/images/Screen Shot 2019-06-23 at 4.03.11 PM.png'}
+      front={'../public/images/Screen Shot 2019-06-23 at 4.03.16 PM.png'}
       flipped={flipped.includes(1)}
       handleClick={() => handleClick(1)}
       />
