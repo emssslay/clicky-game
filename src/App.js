1e795b03815c31ae7b9cae10f9459ab57f9c815b
emssslay <emily.robinson@iechs.org>
2019-06-23 15:56:17 -0600

    somewhat up

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
-import React from "react";
-import Counter from "./components/Counter";
+import React, { useState } from "react";
+import Card from "./components/card";
 
 function App() {
-  return <Counter />;
+  const [flipped, setFlipped] = useState([]);
+  const handleClick = (id) => setFlipped([flipped, id]);
+
+  return (
+    <div>
+      <h2>Can you remember where the cards are?
+      </h2>
+      <Card 
+      id={1}
+      width={100}
+      height={100}
+      back={'./img'}
+      front={'./img'}
+      flipped={flipped.includes(1)}
+      handleClick={() => handleClick(1)}
+      />
+    </div>
+  )
 }
 
 export default App;
