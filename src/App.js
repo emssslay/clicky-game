6df8b3febac6b9ce7ad71334ec0b6e519049caa2
emssslay <emily.robinson@iechs.org>
2019-06-24 18:01:14 -0600

    formatted boxes

--- src/App.js
+++ src/App.js
@@ -6,18 +6,35 @@ import initializeDeck from './deck';
 function App() {
   const [cards, setCards] = useState([]);
   const [flipped,setFlipped] = useState([]);
+  const [dimension, setDimension] = useState(400);
 
   useEffect(() => {
-    setCards(initializeDeck())
+    resizeBoard();
+    setCards(initializeDeck());
   }, []);
 
+  useEffect(() => {
+    const resizeListener = window.addEventListener('resize', resizeBoard);
+
+    return () => window.removeEventListener('resize', resizeListener);
+  });
+
   const handleClick = (id) => setFlipped([ ... flipped, id ])
+
+  const resizeBoard = () => {
+      setDimension(
+        Math.min(
+          document.documentElement.clientWidth,
+          document.documentElement.clientHeight
+        ),
+      )
+  };
 
   return (
     <div>
       <h2>Can you remember where the cards are?
       </h2>
-      <Board cards={cards} flipped={flipped} handleClick={handleClick} />
+      <Board dimension={dimension} cards={cards} flipped={flipped} handleClick={handleClick} />
     </div>
   )
 };
